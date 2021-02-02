1e93fe900f4e971a31c55b2502c5a7a4a308a86f
Asutosh <asutosh.nayak@Asutoshs-MacBook-Pro.local>
2021-02-02 14:17:13 +0530

    dependencies added

---
+++ app/src/main/java/com/outliers/cleancodekt/constants/Const.kt
@@ -0,0 +1,11 @@
+package com.outliers.cleancodekt.constants
+
+class Const {
+
+    companion object{
+        val PAGE_SIZE = 10
+        val INIT_PAGE_NUM = 1
+        val BASE_URL = "https://jsonplaceholder.typicode.com/"
+        val APP_PREF_NAME = "cleanCodeKtAppPref"
+    }
+}
